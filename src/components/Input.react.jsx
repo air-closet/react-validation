4e305348620733e59d8aa5ce5cec141791890d3c
osamu38 <osamu.osamu.0622@gmail.com>
2016-10-21 17:35:13 +0900

    Revert "Change returning DOM architecture"
    This reverts commit ab8dee30c460984b02401eb69ceeefa907deea81.

--- src/components/Input.react.jsx
+++ src/components/Input.react.jsx
@@ -32,7 +32,7 @@ class Input extends Component {
     render() {
         let data = getViewData(this.props);
 
-        return [
+        return <div className={this.props.containerClassName || null}>
             <input
                 ref='node'
                 type='text'
@@ -41,10 +41,9 @@ class Input extends Component {
                 checked={data.props.checked}
                 value={data.value}
                 onChange={this.handleChange.bind(this)}
-                onBlur={this.handleBlur.bind(this)}
-            />,
-            <div>{data.hint}</div>
-        ]
+                onBlur={this.handleBlur.bind(this)}/>
+            {data.hint}
+        </div>
     }
 }
 
